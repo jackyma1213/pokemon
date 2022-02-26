bbe9fd1ffa90a74eb404f69f12f5338a38d89b8c
jackyma1213 <jackyma1213@users.noreply.github.com>
2022-02-26 20:27:53 +0800

    feat: update dictionary

--- src/components/Dictionary.jsx
+++ src/components/Dictionary.jsx
@@ -1,8 +1,27 @@
+import { useQuery } from "@apollo/client";
+import { get } from "lodash";
+import { GET_POKEMONS } from "../graphql/getPokemon";
+import Card from "./Card";
 
 const Dictionary = () => {
-  return (
-    <div>Dictionary</div>
-  )
-}
+  const { loading, error, data } = useQuery(GET_POKEMONS, {
+    variables: { first: 10 },
+  });
 
-export default Dictionary
+  if (loading) return <div>loading...</div>;
+
+  if (error) return <div>error...</div>;
+
+  return (
+    <div className="pt-2 bottom-0 overflow-y-auto ">
+      <div className="flex flex-wrap w-11/12 mx-auto gap-8">
+        {get(data, "pokemons", []).length > 0 &&
+          data.pokemons.map((pokemon) => (
+            <Card data={pokemon} key={pokemon.id} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Dictionary;
