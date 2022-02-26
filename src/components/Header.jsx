8929ec00ab2feb670d21ee74bddfc884950b040a
jackyma1213 <jackyma1213@users.noreply.github.com>
2022-02-27 03:36:02 +0800

    fix: ref issue of Link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const Header = () => {
-  return (
-    <header className="w-screen sticky top-0 z-30 bg-gradient-to-t from-gray-700/95 via-slate-800 to-slate-900">
-      <div className="w-max mx-auto cursor-pointer">
-        <Link href="/">
+const Header = () => (
+  <header className="w-screen sticky top-0 z-30 bg-gradient-to-t from-gray-700/95 via-slate-800 to-slate-900">
+    <div className="w-max mx-auto cursor-pointer">
+      <Link href="/">
+        <div>
           <Image src="/img/pokemonLogo.png" width={250} height={100} />
-        </Link>
-      </div>
-    </header>
-  );
-};
+        </div>
+      </Link>
+    </div>
+  </header>
+);
 
 export default Header;
