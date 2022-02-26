cf873a78df5de3f867876a68c90020c47a4f251c
jackyma1213 <jackyma1213@users.noreply.github.com>
2022-02-27 03:16:36 +0800

    feat: update card link

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,20 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Card = ({ data }) => {
   console.log("data", data);
-  const { name, image } = data;
+  const { name, image, id } = data;
   return (
-    <div className="w-[calc(33.33%-2rem)] h-[500px] hover:scale-105 rounded overflow-hidden shadow-slate-300 shadow-lg bg-white cursor-pointer ">
-      <div className="text-center">
-        <Image src={image} height={300} width={300} />
+    <Link href={`/pokemon/${id}`}>
+      <div className="w-[calc(33.33%-2rem)] h-[500px] hover:scale-105 rounded overflow-hidden shadow-slate-300 shadow-lg bg-white cursor-pointer ">
+        <div className="text-center">
+          <Image src={image} height={300} width={300} />
+        </div>
+        <div className="text-xl text-center text-slate-700 font-bold">
+          {name}
+        </div>
       </div>
-      <div className="text-xl text-center text-slate-700 font-bold">{name}</div>
-    </div>
+    </Link>
   );
 };
 
