31c06fe4a158ac7852bc04df76a6203c8232f960
jackyma1213 <jackyma1213@users.noreply.github.com>
2022-02-27 03:09:41 +0800

    feat: update header style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <div className="w-screen sticky top-0 z-30 bg-gradient-to-b from-indigo-200 via-red-200 to-yellow-100/5">
+    <div className="w-screen sticky top-0 z-30 bg-gradient-to-t from-gray-700/95 via-slate-800 to-slate-900">
       <div className="w-max mx-auto">
         <Image src="/img/pokemonLogo.png" width={250} height={100} />
       </div>
